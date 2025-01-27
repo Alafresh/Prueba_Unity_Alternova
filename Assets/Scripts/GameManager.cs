3ca0c581920f7ba0846f1ae7c0744a05e6e4d1a4
Alafresh <51685896+Alafresh@users.noreply.github.com>
2025-01-27 15:32:01 -0500

    creacion plantilla visual y id de carta

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
-public class Block
+public class Block 
 {
     public int R;
     public int C;
@@ -19,6 +20,9 @@ public class BlocksData
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GridLayoutGroup gridLayoutGroup;
+
+    private List<Card> cards;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,13 +39,36 @@ public class GameManager : MonoBehaviour
             {
                 Debug.Log($"R: {block.R}, C: {block.C}, Number: {block.number}");
             }
+            int numberColumns = GetColumnCount(blocksData.blocks);
+            gridLayoutGroup.constraintCount = numberColumns;
+            int numberRows = GetRowCount(blocksData.blocks);
+            Debug.Log("Numero de columnas es"+ numberColumns);
+            Debug.Log("Numero de filas es" + numberRows);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    int GetRowCount(Block[] blocks)
     {
-        
+        // Usamos un HashSet para almacenar valores únicos de rows
+        HashSet<int> uniqueRows = new HashSet<int>();
+
+        foreach (Block block in blocks)
+        {
+            uniqueRows.Add(block.R); // Agrega la fila al conjunto
+        }
+        return uniqueRows.Count; // El número de rows únicas
+    }
+    
+    int GetColumnCount(Block[] blocks)
+    {
+        // Usamos un HashSet para almacenar valores únicos de columnas
+        HashSet<int> uniqueColumns = new HashSet<int>();
+
+        foreach (Block block in blocks)
+        {
+            uniqueColumns.Add(block.C); // Agrega la columna al conjunto
+        }
+        return uniqueColumns.Count; // El número de columnas únicas
     }
 }
 
